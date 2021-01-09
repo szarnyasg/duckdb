b4c84db7d39c81f3e8790fd7120f9e5a2f167df5
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-10 00:25:21 +0100

    Daily batches PoC

---
+++ benchmark/ldbc/get.sh
@@ -0,0 +1,2 @@
+wget https://github.com/cwida/duckdb/releases/download/v0.2.3/duckdb_cli-linux-amd64.zip
+unzip duckdb_cli-linux-amd64.zip
--- benchmark/ldbc/proc.sh
+++ benchmark/ldbc/proc.sh
@@ -5,8 +5,8 @@ set -e
 export PATHVAR=/home/szarnyasg/git/snb/ldbc-example-graph/snb-example-graph-csv-composite-merge-foreign-raw
 export POSTFIX=_0_0.csv
 
-rm ldbc.duckdb
-cat schema-*.sql      | duckdb ldbc.duckdb
-cat snb-load.sql      | sed "s|PATHVAR|$PATHVAR|" | sed "s|POSTFIX|$POSTFIX|" | duckdb ldbc.duckdb
-cat snb-transform.sql | duckdb ldbc.duckdb
-cat snb-export.sql    | duckdb ldbc.duckdb
+rm -rf ldbc.duckdb
+cat schema-*.sql      | ./duckdb ldbc.duckdb
+cat snb-load.sql      | sed "s|PATHVAR|$PATHVAR|g" | sed "s|POSTFIX|$POSTFIX|g" | ./duckdb ldbc.duckdb
+#cat snb-transform.sql | sed "s|:bulkLoadTime|''|g" | ./duckdb ldbc.duckdb
+cat snb-export.sql    | ./duckdb ldbc.duckdb
